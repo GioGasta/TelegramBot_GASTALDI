614783402156f8a33c95be2ce9c4b1fb58fddca0
geeo_ske <002gastaldi@gmail.com>
2026-01-12 18:32:52 +0100

    feat: wiring for new anime/char commands; add callback constants and DTO scaffolding

--- src/main/java/org/projectATB/telegram/CallbackDataParser.java
+++ src/main/java/org/projectATB/telegram/CallbackDataParser.java
@@ -6,7 +6,16 @@ public class CallbackDataParser {
     public static final String LIST_WATCHED = "LIST:WATCHED";
     public static final String LIST_REMOVE = "LIST:REMOVE";
     
+    public static final String ANIME_VIEW = "ANIME:VIEW";
+    public static final String ANIME_CHAR = "ANIME:CHAR";
+    public static final String ANIME_SEQUEL = "ANIME:SEQUEL";
+    public static final String ANIME_PREQUEL = "ANIME:PREQUEL";
+    public static final String ANIME_STAFF = "ANIME:STAFF";
+    public static final String ANIME_EPISODES = "ANIME:EPISODES";
+    public static final String ANIME_ADD = "ANIME:ADD";
+    public static final String ANIME_WATCHED = "ANIME:WATCHED";
+    
     public static final String ANIME_SEARCH_SELECT = "ANIME_SEARCH:SELECT";
     public static final String ANIME_SEARCH_RETRY = "ANIME_SEARCH:RETRY";
 
-}
+ }
